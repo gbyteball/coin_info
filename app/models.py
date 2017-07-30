803a5a248d6e97cac56cb8c60b25791ebc834684
jw.jang <girlriver@gmail.com>
2017-07-30 14:07:52 +0900

    DB iswallet 컬럼 추가
    화면에서 지갑 구분 로직 단순화

--- app/models.py
+++ app/models.py
@@ -8,18 +8,20 @@ class CoinInfo(Base):
   image_name = Column(String(11))
   code = Column(String(10))
   place = Column(String(20))
+  iswallet = Column(String(2))
   amount = Column(Float)
   price_init = Column(Integer)
   memo = Column(String(200))
   
-  def __init__(self, name, image_name, code, place, amount, price_init, memo):
+  def __init__(self, name, image_name, code, place, iswallet, amount, price_init, memo):
     self.name = name
     self.image_name = image_name
     self.code = code
     self.place = place
+    self.iswallet = iswallet
     self.amount = amount
     self.price_init = price_init
     self.memo = memo
     
   def __repr__(self):
-    return "<CoinInfo('%d', '%s', '%s', '%s', '%s', '%f', '%d','%s'>" %(self.id, self.name, self.image_name, self.code, self.place, self.amount, self.price_init, self.memo)
+    return "<CoinInfo('%d', '%s', '%s', '%s', '%s', '%s', '%f', '%d','%s'>" %(self.id, self.name, self.image_name, self.code, self.place, self.iswallet, self.amount, self.price_init, self.memo)
