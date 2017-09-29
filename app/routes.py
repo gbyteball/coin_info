53b65c32d14c29f2cf54a9087ab0a9de0d267419
jw.jang <girlriver@gmail.com>
2017-09-29 14:55:49 +0900

    ..

--- app/routes.py
+++ app/routes.py
@@ -8,10 +8,6 @@ from bittrex.bittrex import Bittrex
 import json
 import multiprocessing
 from multiprocessing import Process, Queue
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 
 
 app = Flask(__name__)
@@ -31,10 +27,6 @@ def coin2():
   q = multiprocessing.Queue()
 
   # coin_price
-<<<<<<< HEAD
-=======
-  bittrexCoinList = ['ETH', 'XRP', 'BCC', 'PAY', 'GBYTE', 'EDG', 'SNT', 'ADX', 'OMG', 'DASH', 'ZEC', 'GNT', 'NXT', 'STRAT', 'QTUM', 'KMD', 'NMR', 'NEO', 'ANS']
->>>>>>> origin/master
   for coindict in result:
     if coindict['code'] == 'BTC':
       p = multiprocessing.Process(target=getBittrexPrice2, args=('USDT', coindict['code'], 'Last', q))
@@ -53,111 +45,6 @@ def coin2():
       procs.append(p)
       p.start()
     else:
-<<<<<<< HEAD
-=======
-      coindict['coin_price'] = 0
-    
-
-# price_now
-# 10kKRW_coin_amount
-# asset_rate
-
-
-  bPrice = Bittrex()
-  bittrexBalancesDict = Bittrex.get_balances(bPrice)
-  if bittrexBalancesDict['success'] == True:
-    for coindict in result:
-      if coindict['place'] == 'bittrex':
-        for h in bittrexBalancesDict['result']:
-          if coindict['code'] == h['Currency']:
-            coindict['amount'] = h['Balance']
-#            print('amount reset : ', h['Currency'], h['Balance'])
-
-  
-  pBTCWon = pUSD*pBTC
-  pETHWon = pUSD*pETH*pBTC
-  pXRPWon = pUSD*pXRP*pBTC
-
-
-  # profit_percent
-  for coindict in result:
-    if coindict['amount'] != 0 and coindict['price_init'] != 0:
-      if coindict['name'] == 'Bitcoin':
-        coindict['profit_percent'] = ((pBTCWon * coindict['amount']) - coindict['price_init'])/coindict['price_init']*100
-      elif coindict['name'] == 'Ethereum':
-        coindict['profit_percent'] = ((pETHWon * coindict['amount']) - coindict['price_init'])/coindict['price_init']*100
-      elif coindict['name'] == 'Ripple':
-        coindict['profit_percent'] = ((pXRPWon * coindict['amount']) - coindict['price_init'])/coindict['price_init']*100
-      elif coindict['name'] != 'DAO.Casino' and coindict['name'] != 'Tezos':
-        coindict['profit_percent'] = ((coindict['coin_price'] * coindict['amount'] * pBTCWon) - coindict['price_init'])/coindict['price_init']*100
-      else:
-        coindict['profit_percent'] = 0
-    else:
-      coindict['profit_percent'] = 0
-
-
-  # profit_value
-  for coindict in result:
-    if coindict['name'] == 'USD':
-      coindict['profit_value'] = int((pUSD * coindict['amount']) - coindict['price_init'])
-    elif coindict['name'] == 'Bitcoin':
-      coindict['profit_value'] = int((pBTCWon * coindict['amount']) - coindict['price_init'])
-    elif coindict['name'] == 'Ethereum':
-      coindict['profit_value'] = int((pETHWon * coindict['amount']) - coindict['price_init'])
-    elif coindict['name'] == 'Ripple':
-      coindict['profit_value'] = int((pXRPWon * coindict['amount']) - coindict['price_init'])
-    elif coindict['name'] != 'DAO.Casino' and coindict['name'] != 'Tezos':
-      coindict['profit_value'] = int((coindict['coin_price'] * coindict['amount'] * pBTCWon) - coindict['price_init'])
-    else:
-      coindict['profit_value'] = 0
-
-
-  total_profit_value = sum([i['profit_value'] for i in result])
-  total_price_init = sum([i['price_init'] for i in result])
-  refreshSec = request.args.get('refresh')
-  
-  return render_template('coin.html', result = result, pUSD = pUSD, pBTC = pBTC, pBTCWon = pBTCWon, pETH = pETH, pETHWon = pETHWon, pXRP = pXRP, pXRPWon = pXRPWon, total_profit_value = total_profit_value, total_price_init = total_price_init, refreshSec = refreshSec)
-
-
-
-
-
-
-
-@app.route('/coin2')
-def coin2():
-  result = show_tables()
-
-
-  pUSD = 0.0
-  pBTC = 0.0
-  pETH = 0.0
-  pXRP = 0.0
-
-  procs = []
-  multiThreadResult = list()
-  q = multiprocessing.Queue()
-
-  # coin_price
-  for coindict in result:
-    if coindict['code'] == 'BTC':
-      p = multiprocessing.Process(target=getBittrexPrice2, args=('USDT', coindict['code'], 'Last', q))
-      procs.append(p)
-      p.start()
-    elif coindict['code'] == 'USDT':
-      p = multiprocessing.Process(target=getUSDKRW2, args=('', q))
-      procs.append(p)
-      p.start()
-    elif coindict['place'] == 'bittrex':
-      p = multiprocessing.Process(target=getBittrexPrice2, args=('BTC', coindict['code'], 'Last', q))
-      procs.append(p)
-      p.start()
-    elif coindict['place'] == 'bitfinex':
-      p = multiprocessing.Process(target=getBitfinexPrice, args=('BTC', coindict['code'], 'last_price', q))
-      procs.append(p)
-      p.start()
-    else:
->>>>>>> origin/master
       q.put([coindict['code'], 0])
     
   for p in procs:
@@ -174,11 +61,7 @@ def coin2():
         elif coindict['code'] == 'USDT':
           pUSD = coindict['coin_price']
           if(pUSD == 0):
-<<<<<<< HEAD
 	        pUSD = 1140.2
-=======
-	        pUSD = 1125
->>>>>>> origin/master
         elif coindict['code'] == 'ETH':
           pETH = coindict['coin_price']
         elif coindict['code'] == 'XRP':
@@ -265,11 +148,7 @@ def coin2():
       coindict['profit_value'] = int((pETHWon * coindict['amount']) - coindict['price_init'])
     elif coindict['name'] == 'Ripple':
       coindict['profit_value'] = int((pXRPWon * coindict['amount']) - coindict['price_init'])
-<<<<<<< HEAD
     elif coindict['name'] != 'DAO.Casino' and coindict['name'] != 'BlackMoon' and coindict['name'] != 'Tezos' and coindict['coin_price']:
-=======
-    elif coindict['name'] != 'DAO.Casino' and coindict['name'] != 'Tezos' and coindict['coin_price']:
->>>>>>> origin/master
       coindict['profit_value'] = int((coindict['coin_price'] * coindict['amount'] * pBTCWon) - coindict['price_init'])
     else:
       coindict['profit_value'] = 0
